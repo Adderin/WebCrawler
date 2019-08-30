73810f7e6be7fee6b26d01603631aeb9e367c416
Adel <34869007+Adderin@users.noreply.github.com>
2019-08-30 23:11:13 +0200

    Add files via upload

--- spring-demo1/TrackCoach.java
+++ spring-demo1/TrackCoach.java
@@ -2,9 +2,26 @@ package com.adel2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+	private FortuneService FortuneService;
+	
+	
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.FortuneService = fortuneService;
+	}
+
+	public TrackCoach() {
+	}
+
 	@Override
 	public String getDailyWorkout() {
 
 		return "Run a hard 5K";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "Just Do it: " + FortuneService.getFortune();
 	}
 }
